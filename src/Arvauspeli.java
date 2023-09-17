be1023c2a45b329e6f76bb75a0e07e814668a7f4
Jonna Sääskö <jonna.saasko@student.hamk.fi>
2023-09-17 09:48:50 +0300

    Initial commit

---
+++ src/Arvauspeli.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Arvauspeli {
+
+	public static void main(String[] args) {
+		// This is a game, where user tries to guess a name.
+		// You can guess as many times as you wish or leave the game.
+		
+		Scanner in = new Scanner (System.in);
+		
+		String rightAnswer = "Anton";
+		
+		System.out.println("Guess the name. If you wish to leave, write exit.");
+
+	}
+
+}
